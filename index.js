3b582cbef5318a904282b42ad9c9435f2627f27d
Hadeghe Hadeghe <hadeghehadeghe@Hadeghes-MacBook-Pro.local>
2021-08-12 14:20:46 -0700

    Added a script on index.html

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+// var, let, or const indicates a declaration
+// when you see '=' it indicates an assignment
+var firstName = "Meriem";
+let lastName = "Idris";
+// You cannot register constant variable!
+const age = "18";
+// Variable reassignment
+
+firstName = "Aizawa's"
+lastName = "Wife"
+
+console.log('Hello '   + firstName + ' ' + lastName + '!' + ' you are '  + age + ' years old!');
